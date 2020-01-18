4027363841fcbf9ef81d66e0dee0b53f1ab0529f
Amanda Filizola <amandapaivafilizola@gmail.com>
2020-01-18 17:37:00 -0300

    Add cors() to server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,8 @@ mongoose.connect('mongodb+srv://semana10:omnistack@cluster0-pfp1o.mongodb.net/se
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+// app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
